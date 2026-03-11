dbd176595779ffd47afee2a9671eaadf20456db7
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2026-03-11 18:31:28 +0500

    updates

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -2,7 +2,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import connectDB from '@/lib/mongodb'
 import User from '@/models/User'
 
-export const authOptions = {
+const authOptions = {
   providers: [
     CredentialsProvider({
       name: 'credentials',
