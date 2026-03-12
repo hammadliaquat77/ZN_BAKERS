76d44e02bcf8fd252250cc332bf531222ebf789d
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2026-03-12 17:00:45 +0500

    upload api changes

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,7 +1,8 @@
 
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/authOptions'
 import cloudinary from '@/lib/cloudinary'
 
 async function uploadToCloudinary(buffer) {
